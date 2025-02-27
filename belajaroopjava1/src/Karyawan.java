98fd323feff62ca584f155f6fd367f630e48df04
RijalPratama21 <rijal.pratama_ti23@nusaputra.ac.id>
2025-02-27 23:21:48 +0700

    menambahkan beberapa file baru

---
+++ belajaroopjava1/src/Karyawan.java
@@ -0,0 +1,36 @@
+public class Karyawan {
+    String nama;
+    double gajiPokok;
+    double tunjangan;
+
+    // ini konstructor pertama dengan 3 paramenetr
+    public Karyawan(String nama, double gapok, double tunjangan) {
+        this.nama = nama;
+        gajiPokok = gapok;
+        this.tunjangan = tunjangan;
+    }
+
+    // constructor kedua dengan 2 parameter
+    public Karyawan(String nama, double tunjangan) {
+        this.nama = nama;
+        this.tunjangan = tunjangan;
+    }
+
+    // SET data kryawan
+    public void setDataKaryawan(String nm, double gp, double tj) {
+        nama = nm;
+        gajiPokok = gp;
+        tunjangan = tj;
+    }
+
+    // Get
+    public String getNama() {
+        return nama;
+    }
+    public double getGajiPokok() {
+        return gajiPokok;
+    }
+    public double getTunjangan() {
+        return tunjangan;
+    }
+}
---
+++ belajaroopjava1/src/KaryawanKontrak.java
@@ -0,0 +1,25 @@
+public class KaryawanKontrak extends Karyawan {
+    double gajiPerHari;
+    int jmlMasuk;
+    public KaryawanKontrak(String nama, double tunjangan){
+        super(nama,tunjangan);
+        gajiPerHari = 20000;
+    }
+
+    public void setKaryawanKontrak(double gph, int jmlMasuk) {
+        gajiPerHari = gph;
+        this.jmlMasuk = jmlMasuk;
+    }
+
+    public double getGaji() {
+        double total = (gajiPerHari * jmlMasuk) + super.getTunjangan();
+        return total;
+    }
+
+    public void infoKaryawan() {
+        System.out.println("Nama :" + super.getNama());
+        System.out.println("Gaji/Hari :" + gajiPerHari);
+        System.out.println("Jumlah Masuk :" + jmlMasuk);
+        System.out.println("Total Gaji :" + getGaji());
+    }
+}
