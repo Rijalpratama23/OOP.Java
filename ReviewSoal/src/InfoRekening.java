8494e713094509b60d78bb309c5bc222345a84e0
RijalPratama21 <rijal.pratama_ti23@nusaputra.ac.id>
2025-03-14 04:29:12 +0700

    edit file

--- ReviewSoal/src/InfoRekening.java
+++
@@ -1,9 +0,0 @@
-import java.util.Scanner;
-
-public class InfoRekening {
-    Scanner sc = new Scanner(System.in);
-
-    // menampilkan pilihan menu 
-        
-        int pilihan = sc.nextInt();
-}
