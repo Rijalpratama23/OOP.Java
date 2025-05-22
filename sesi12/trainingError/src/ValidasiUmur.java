185af063dba46ce2984edb9d40c6faf3692f2c3c
RijalPratama21 <rijal.pratama_ti23@nusaputra.ac.id>
2025-05-22 14:21:28 +0700

    menambah file dan upgrade file

--- sesi12/trainingError/src/ValidasiUmur.java
+++ sesi12/trainingError/src/ValidasiUmur.java
@@ -1,3 +1,7 @@
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class ValidasiUmur{
     
     public void cekUmur(int umur)  throws ValidasiException  {
@@ -6,5 +10,23 @@ public class ValidasiUmur{
         }
     }
 
-    
+    public static void main(String[] args) {
+        ValidasiUmur uv = new ValidasiUmur();
+        Scanner sc = new Scanner(System.in);
+
+        try {
+            System.out.println("umur: ");
+            int umur = sc.nextInt();
+            uv.cekUmur(umur);
+        }catch(InputMismatchException ie){
+            System.out.println("input Anda Salah!");
+        }
+        catch (ValidasiException e) {
+            System.out.println(e.getMessage());
+        }
+        finally{
+            sc.close();
+        }
+    }
+
 }
